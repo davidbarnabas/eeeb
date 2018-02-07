5c5441bf3f63b7164747e3534627f67e2337e805
davidbarnabas <35915198+davidbarnabas@users.noreply.github.com>
2018-02-07 14:48:03 +0530

    Create db23.c

---
+++ db23.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int a[20],i,n,min;
+
+printf("enter the number:\n");
+scanf("%d",&n);
+printf("enter the %d integer in an array:\n",n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+min=a[0];
+for(i=1;i<n;i++)
+{
+if(min>a[i])
+{
+min=a[i];
+}
+}
+printf("The smallest number in an array is:%d\n",min);
+return 0;
+}
